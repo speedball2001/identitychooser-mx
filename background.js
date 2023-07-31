6400f221c8b462c05ce3d721421e71d82b9bd72b
speedball <janek@janek.org>
2023-07-31 14:43:28 +0200

    MOD: Renamed background2.js to background.js.

--- background.js
+++ background.js
@@ -3,11 +3,13 @@ import { IcIdentities } from '../modules/identities.js';
 
 class IdentityChooser {
   constructor() {
+    this.activeIdentityWindows = [];
+
     this.icOptions = new Options();
   }
 
   async run() {
-    console.debug("IdentityChooser#run -- begin");
+    console.log("Identity Chooser#run");
 
     try {
       await this.icOptions.setupDefaultOptions();
@@ -29,208 +31,148 @@ class IdentityChooser {
       browser.runtime.reload();
     }
 
-    browser.icApi.onIdentityChosen.addListener(
-        (identityId, action, windowId, info) => this.identityChosen(identityId, action, windowId, info));
-    console.debug('IdentityChooser#run: onIdentityChosen listener registered');
-
-    console.debug('IdentityChooser#run: iterate over accounts and identities');
-    var icIdentities = new IcIdentities(this.icOptions);
-    var identities = await icIdentities.getIdentities();
-
-    for (const identity of identities) {
-      if (identity !== undefined){
-        console.debug(`IdentityChooser#run: found ${identity.accountId}, ${identity.id}`);
-        if (identity.showInMenu) {
-          var icIdentity = {
-            "id": identity.id,
-            "label": identity.label
-          }
-
-          var isEnabledComposeMessage =
-            await this.icOptions.isEnabledComposeMessage();
-          if (isEnabledComposeMessage) {
-            console.debug('IdentityChooser#run: add identity ',
-              icIdentity,
-              'to compose');
-            browser.icApi.addIdentity(icIdentity, "compose");
-          }
-
-          var isEnabledReplyMessage =
-            await this.icOptions.isEnabledReplyMessage();
-          if (isEnabledReplyMessage) {
-            console.debug('IdentityChooser#run: add identity ',
-              icIdentity,
-              'to reply');
-            browser.icApi.addIdentity(icIdentity, "reply");
-
-            console.debug('IdentityChooser#run: add identity ',
-              icIdentity,
-              'to replyAll');
-            browser.icApi.addIdentity(icIdentity, "replyAll");
-          }
-
-          var isEnabledForwardMessage =
-            await this.icOptions.isEnabledForwardMessage();
-          if (isEnabledForwardMessage) {
-            console.debug('IdentityChooser#run: add identity ',
-              icIdentity,
-              'to forward');
-            browser.icApi.addIdentity(icIdentity, "forward");
-          }
-        }
-      }
-    }
-
-    //
-    // initialize UI of all open windows
-    browser.windows.getCurrent().then((window) => this.initUI(window));
-
-    //
-    // listen to new window create events to init their UI
-    browser.windows.onCreated.addListener((window) => this.initUI(window));
-    console.debug('IdentityChooser#run: browser.windows.onCreated listener registered');
-    console.debug("IdentityChooser#run -- end");
+    browser.tabs.onCreated.addListener(async (tab) => this.tabCreated(tab));
   }
 
-  async initUI(window) {
-    console.debug("IdentityChooser#initUI -- begin");
-    console.debug(`IdentityChooser#initUI: window: ${window.type}`);
+  async tabCreated(tab) {
+    if(tab.type != 'messageCompose') {
+      return;
+    }
 
-    if(window.type == "normal" ||
-       window.type == "messageDisplay") {
+    let composeDetails = await browser.compose.getComposeDetails(tab.id);
+
+    if(composeDetails.type == "new") {
       var isEnabledComposeMessage =
           await this.icOptions.isEnabledComposeMessage();
-      if(isEnabledComposeMessage) {
-        console.debug("IdentityChooser#initUI: initComposeMessageAction");
-        browser.icApi.initComposeMessageAction(window.id);
-      }
 
+      if(!isEnabledComposeMessage) {
+        return;
+      }
+    }
+
+    if(composeDetails.type == "reply") {
       var isEnabledReplyMessage =
           await this.icOptions.isEnabledReplyMessage();
-      if(isEnabledReplyMessage) {
-        console.debug("IdentityChooser#initUI: initReplyMessageAction");
-        browser.icApi.initReplyMessageAction(window.id);
-      }
 
+      if(!isEnabledReplyMessage) {
+        return;
+      }
+    }
+
+    if(composeDetails.type == "forward") {
       var isEnabledForwardMessage =
           await this.icOptions.isEnabledForwardMessage();
-      if(isEnabledForwardMessage) {
-        console.debug("IdentityChooser#initUI: initForwardMessageAction");
-        browser.icApi.initForwardMessageAction(window.id);
+
+      if(!isEnabledForwardMessage) {
+        return;
       }
     }
 
-    console.debug("IdentityChooser#initUI -- end");
+    let identityWindow = await browser.windows.create({
+      url: "identitypopup/popup.html",
+      type: "popup",
+      height: 300,
+      width: 450,
+      allowScriptsToClose: true,
+    });
+
+    let idFocusListener = async (windowId) => this.focusChanged(windowId,
+                                                                identityWindow.id);
+    let idRemovedListener = async (windowId) => this.windowRemoved(windowId,
+                                                                   identityWindow.id);
+
+    let activeIdentityWindow = {
+      identityWindowId: identityWindow.id,
+      composeWindowId: tab.windowId,
+      removedListener: idRemovedListener,
+      focusListener: idFocusListener,
+    };
+
+    this.activeIdentityWindows.push(activeIdentityWindow);
+
+    console.log(this.activeIdentityWindows);
+
+    browser.windows.onFocusChanged.addListener(
+      activeIdentityWindow.focusListener);
+    browser.windows.onRemoved.addListener(
+      activeIdentityWindow.removedListener);
+
+    let chosenIdentity = await this.popupPrompt(identityWindow.id, null);
+
+    if(chosenIdentity != null) {
+      browser.compose.setComposeDetails(tab.id, { identityId: chosenIdentity });
+    }
   }
 
-  async identityChosen(identityId, action, windowId, info) {
-    console.debug('IdentityChooser#identityChosen -- begin');
-    console.debug(`IdentityChooser#identityChosen: identityId: ${identityId}, action: ${action}, windowId: ${windowId}, info: ${info}`);
+  async windowRemoved(windowId, identityWindowId) {
+    console.log("windowRemoved: " + windowId + "|" + identityWindowId);
 
-    var messageFormat = await browser.composePrefsApi.getMessageFormat(identityId);
+    let identityWindow = this.activeIdentityWindows.find(e => e.identityWindowId == identityWindowId);
+    let composeWindowId = identityWindow.composeWindowId;
 
-    if(info.includes("Shift")) {
-      if(messageFormat == "text/plain") {
-        messageFormat = "text/html";
-      } else {
-        messageFormat = "text/plain";
-      }
+    if(windowId == identityWindowId) {
+      // identity window closed
+      browser.windows.onFocusChanged.removeListener(identityWindow.focusListener);
+      browser.windows.onRemoved.removeListener(identityWindow.removedListener);
+
+      let idx = this.activeIdentityWindows.findIndex(e => e.identityWindowId == identityWindowId);
+      this.activeIdentityWindows.splice(idx, 1);
+
+      console.log(this.activeIdentityWindows);
     }
 
-    console.debug('IdentityChooser#identityChosen: messageFormat: ', messageFormat);
-    if(action == "compose") {
-      console.debug('IdentityChooser#identityChosen: open compose editor');
-      if(messageFormat == "text/plain") {
-        browser.compose.beginNew({
-          "identityId": identityId,
-          "isPlainText" : true
-        });
-      } else {
-        browser.compose.beginNew({
-          "identityId": identityId,
-          "isPlainText" : false
-        });
-      }
-    } else if(action == "reply") {
-      var tabs = await browser.tabs.query({ "windowId": windowId, "active": true });
-      for (let tab of tabs) {
-        var msg = await browser.messageDisplay.getDisplayedMessage(tab.id);
+    if(windowId == composeWindowId) {
+      // composer window closed
+      console.log("composer closed");
 
-        if(msg != null) {
-          console.debug('IdentityChooser#identityChosen: reply to message: ', msg);
+      browser.windows.remove(identityWindowId);
+    }
+  }
 
-          if(messageFormat == "text/plain") {
-            browser.compose.beginReply(msg.id,
-                                       "replyToSender",
-                                       { "identityId": identityId,
-                                         "isPlainText" : true
-                                       });
-          } else {
-            browser.compose.beginReply(msg.id,
-                                       "replyToSender",
-                                       { "identityId": identityId,
-                                         "isPlainText" : false
-                                       });
-          }
-        }
-      }
-    } else if(action == "replyAll") {
-      var tabs = await browser.tabs.query({ "windowId": windowId, "active": true });
-      for (let tab of tabs) {
-        var msg = await browser.messageDisplay.getDisplayedMessage(tab.id);
+  async focusChanged(windowId, identityWindowId) {
+    let identityWindow = this.activeIdentityWindows.find(e => e.identityWindowId == identityWindowId);
+    let composeWindowId = identityWindow.composeWindowId;
 
-        console.debug('IdentityChooser#identityChosen: reply all to  message: ',
-                      msg);
-        if(msg != null) {
-          if(messageFormat == "text/plain") {
-            browser.compose.beginReply(msg.id,
-                                       "replyToAll",
-                                       { "identityId": identityId,
-                                         "isPlainText" : true
-                                       });
-          } else {
-            browser.compose.beginReply(msg.id,
-                                       "replyToAll",
-                                       { "identityId": identityId,
-                                         "isPlainText" : false
-                                       });
-          }
-        }
-      }
-    } else if(action == "forward") {
-      var forwardType = await browser.composePrefsApi.getForwardType();
-      console.debug('IdentityChooser#identityChosen: forwardType: ',
-                    forwardType);
-
-      var tabs = await browser.tabs.query({ "windowId": windowId, "active": true });
-      for (let tab of tabs) {
-        var msg = await browser.messageDisplay.getDisplayedMessage(tab.id);
-        var window = await browser.windows.getCurrent();
-
-        console.debug('IdentityChooser#identityChosen: forward  message: ',
-                      msg);
-        if(msg != null) {
-          if(!info.includes("Shift")) {
-            browser.icForwardApi.beginForward(msg,
-                                              forwardType,
-                                              { "identityId": identityId,
-                                                "format" : "Default"
-                                              });
-          } else {
-            browser.icForwardApi.beginForward(msg,
-                                              forwardType,
-                                              { "identityId": identityId,
-                                                "format" : "OppositeOfDefault"
-                                              });
-          }
-        }
-      }
+    if(windowId == composeWindowId) {
+      browser.windows.update(identityWindowId, { focused: true });
     }
 
-    console.debug('IdentityChooser#identityChosen -- end');
+    console.log("windowId: " + windowId);
+    console.log("composeWindowId: " + composeWindowId);
+    console.log("identityWindowId: " + identityWindowId);
+  }
+
+  async popupPrompt(popupId, defaultResponse) {
+    try {
+      await messenger.windows.get(popupId);
+    } catch (e) {
+      // Window does not exist, assume closed.
+      return defaultResponse;
+    }
+
+    return new Promise(resolve => {
+      let response = defaultResponse;
+      function windowRemoveListener(closedId) {
+	if (popupId == closedId) {
+	  messenger.windows.onRemoved.removeListener(windowRemoveListener);
+	  messenger.runtime.onMessage.removeListener(messageListener);
+          resolve(response);
+	}
+      }
+      function messageListener(request, sender, sendResponse) {
+        if (sender.tab.windowId != popupId || !request) {
+          return;
+        }
+
+        if (request.popupResponse) {
+          response = request.popupResponse;
+        }
+      }
+      messenger.runtime.onMessage.addListener(messageListener);
+      messenger.windows.onRemoved.addListener(windowRemoveListener);
+    });
   }
 }
-
 
 async function waitForLoad() {
   let onCreate = new Promise(function(resolve, reject) {
